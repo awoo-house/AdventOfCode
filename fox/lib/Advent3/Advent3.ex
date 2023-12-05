1129bb183b53aa5a42e57dde41ea25874e3ca6bc
Fox Huston <448633+foxhuston@users.noreply.github.com>
2023-12-05 08:52:07 -0500

    Processing input lines correctly

--- fox/lib/Advent3/Advent3.ex
+++ fox/lib/Advent3/Advent3.ex
@@ -1,3 +1,22 @@
 defmodule Advent3 do
+  alias Advent3.CoordinateSymbol
+  alias Advent3.LineProcessor
 
+  @spec processLines(String.t()) :: [{ [CoordinateSymbol], integer() }]
+  def processLines(inp) do
+    String.split(inp, "\n")
+    |> Enum.map(&String.trim/1)
+    |> Enum.filter(fn l -> String.length(l) > 0 end)
+    |> Enum.map(&LineProcessor.processLine/1)
+    |> Enum.with_index
+  end
+
+
+  # def run do
+  #   case File.read("./lib/Puzz3.input.txt") do
+  #     {:error, reason} -> IO.puts("File read failed because: " <> reason)
+  #     {:ok, input} ->
+  #       IO.puts(findNumbers(input))
+  #   end
+  # end
 end
--- fox/test/Advent3_test.exs
+++ fox/test/Advent3_test.exs
@@ -2,4 +2,34 @@ defmodule Advent3Test do
   use ExUnit.Case
   doctest Advent3
 
+  alias Advent3.CoordinateSymbol, as: C
+
+  test "Processes Lines" do
+    out = Advent3.processLines("""
+      467..114..
+      ...*......
+      ..35..633.
+      ......#...
+      617*......
+      .....+.58.
+      ..592.....
+      ......755.
+      ...$.*....
+      .664.598..
+    """)
+
+    assert out == [
+      { [%C{ column: 0, num: 467 }, %C{ column: 5, num: 114 }] , 0 },
+      { [%C{ column: 3, sym: ?* }]                             , 1 },
+      { [%C{ column: 2, num: 35 },  %C{ column: 6, num: 633}]  , 2 },
+      { [%C{ column: 6, sym: ?# }]                             , 3 },
+      { [%C{ column: 0, num: 617 }, %C{ column: 3, sym: ?* }]  , 4 },
+      { [%C{ column: 5, sym: ?+ },  %C{ column: 7, num: 58 }]  , 5 },
+      { [%C{ column: 2, num: 592 }]                            , 6 },
+      { [%C{ column: 6, num: 755 }]                            , 7 },
+      { [%C{ column: 3, sym: ?$ },  %C{ column: 5, sym: ?* }]  , 8 },
+      { [%C{ column: 1, num: 664 }, %C{ column: 5, num: 598}]  , 9 },
+    ]
+  end
+
 end
