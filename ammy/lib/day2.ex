f1fa4eed3d991ebdd7f88b90479160c792395c03
Robin Reed <robin.reed@sonatus.com>
2023-12-05 12:49:42 -0800

    Two two

--- ammy/lib/day2.ex
+++ ammy/lib/day2.ex
@@ -17,26 +17,36 @@ defmodule AmmyTwo do
         [count_str, color] = String.split(str, " ")
         count = String.to_integer(count_str)
 
-        Map.update(map, color, count, fn current_count ->
-          if count > current_count, do: count, else: current_count
-        end)
+        map |> Map.update(color, count, fn val -> max(val, count) end)
       end)
 
-    if Map.get(results, "green") > green_thresh or
-         Map.get(results, "red") > red_thresh or
-         Map.get(results, "blue") > blue_thresh do
-      false
-    else
-      true
-    end
+    valid_game =
+      Map.get(results, "green") <= green_thresh and
+        Map.get(results, "red") <= red_thresh and
+        Map.get(results, "blue") <= blue_thresh
+
+    min_products =
+      results
+      |> Enum.map(fn {_, prod} -> prod end)
+      |> Enum.reduce(1, fn n, acc -> n * acc end)
+
+    {valid_game, min_products}
   end
 
-  def process_games(g) do
+  def find_valid_games(g) do
     g
     |> Enum.map(&process_game/1)
     |> Enum.with_index()
-    |> Enum.filter(fn {result, _index} -> result end)
+    |> Enum.filter(fn {result, _index} -> elem(result, 0) end)
     |> Enum.map(fn {_result, index} -> index + 1 end)
+    |> Enum.reduce(0, &(&1 + &2))
+  end
+
+  def find_sum_of_powers(g) do
+    g
+    |> Enum.map(&process_game/1)
+    |> Enum.filter(fn result -> elem(result, 1) end)
+    |> Enum.map(fn {_, index} -> index end)
     |> Enum.reduce(0, &(&1 + &2))
   end
 
@@ -45,7 +55,12 @@ defmodule AmmyTwo do
       {:ok, input} ->
         input
         |> String.split("\n", trim: true)
-        |> process_games()
+        |> find_valid_games()
+        |> IO.inspect()
+
+        input
+        |> String.split("\n", trim: true)
+        |> find_sum_of_powers()
         |> IO.inspect()
 
       {:error, reason} ->
