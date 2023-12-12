9f294b70fe3ea39c28aa7695b9bde50b99d572ef
Robin Reed <robin.reed@sonatus.com>
2023-12-12 13:44:33 -0800

    update blogablog

--- ammy/lib/day6.ex
+++ ammy/lib/day6.ex
@@ -1,18 +1,20 @@
 defmodule AmmySix do
   def read_times_and_distances(content) do
-    lines = String.split(content, "\n", trim: true)
     # for part one
     # times_line = String.replace(Enum.at(lines, 0), ~r/[^\s\d]+/, "")
-    times_line = String.replace(Enum.at(lines, 0), ~r/[^\d]+/, "")
-    times_line = String.split(times_line, ~r/\s+/, trim: true)
-
     # distances_line = String.replace(Enum.at(lines, 1), ~r/[^\s\d]+/, "")
-    distances_line = String.replace(Enum.at(lines, 1), ~r/[^\d]+/, "")
-    distances_line = String.split(distances_line, ~r/\s+/, trim: true)
+    lines = String.split(content, "\n", trim: true)
 
-    times = Enum.map(times_line, &Integer.parse/1)
+    times =
+      String.replace(Enum.at(lines, 0), ~r/[^\d]+/, "")
+      |> String.split(~r/\s+/, trim: true)
+      |> Enum.map(&Integer.parse/1)
+    distances =
+      String.replace(Enum.at(lines, 1), ~r/[^\d]+/, "")
+      |> String.split(~r/\s+/, trim: true)
+      |> Enum.map(&Integer.parse/1)
+
     times_only = for {n, _} <- times, do: n
-    distances = Enum.map(distances_line, &Integer.parse/1)
     distances_only = for {n, _} <- distances, do: n
 
     {times_only, distances_only}
