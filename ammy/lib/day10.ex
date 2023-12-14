8da91942e4e243fb15acc5aff4632e34b1fd94ac
Robin Reed <robin.reed@sonatus.com>
2023-12-13 21:40:18 -0800

    Ten part two

--- ammy/lib/day10.ex
+++ ammy/lib/day10.ex
@@ -34,9 +34,11 @@ defmodule AmmyTen do
         connector_at_pos = Enum.at(map, row) |> Enum.at(col)
         connects_to = Map.get(connections, connector_at_pos)
 
-        new_q = Enum.reduce(connects_to, q, fn {dy, dx}, queue ->
-          queue ++ [dist + 1, {dy + col, dx + row}]
-        end)
+        new_q =
+          Enum.reduce(connects_to, q, fn {dy, dx}, queue ->
+            queue ++ [dist + 1, {dy + col, dx + row}]
+          end)
+
         process_queue(new_q, new_dists, map, connections)
       else
         process_queue(q, dists, map, connections)
@@ -58,43 +60,98 @@ defmodule AmmyTen do
 
     offsets = [{-1, 0}, {1, 0}, {0, -1}, {0, 1}]
 
-    shitty_queue = Enum.reduce(offsets, [], fn {dx, dy}, acc ->
-      c =
-        Enum.fetch(map, row + dy)
-        |> case do
-          {:ok, row_values} -> Enum.fetch(row_values, col + dx)
-          _ -> {:error, nil}
-        end
-
-      case c do
-        {:ok, cell} ->
-          new_coords =
-            Enum.filter(Map.get(connections, cell), fn {dx2, dy2} ->
-              col == col + dx + dx2 && row == row + dy + dy2
-            end)
-
-          if length(new_coords) > 0 do
-            [1, {col + dx, row + dy} | acc]
-          else
-            acc
+    shitty_queue =
+      Enum.reduce(offsets, [], fn {dx, dy}, acc ->
+        c =
+          Enum.fetch(map, row + dy)
+          |> case do
+            {:ok, row_values} -> Enum.fetch(row_values, col + dx)
+            _ -> {:error, nil}
           end
-        _ -> acc
-      end
-    end)
+
+        case c do
+          {:ok, cell} ->
+            new_coords =
+              Enum.filter(Map.get(connections, cell), fn {dx2, dy2} ->
+                col == col + dx + dx2 && row == row + dy + dy2
+              end)
+
+            if length(new_coords) > 0 do
+              [1, {col + dx, row + dy} | acc]
+            else
+              acc
+            end
+
+          _ ->
+            acc
+        end
+      end)
 
     dists = %{%Coord{col: col, row: row} => 0}
 
     process_queue(shitty_queue, dists, map, connections)
   end
 
+  # seven arguments is a reasonable number yes
+  # owo
+  def fill_insides(map, dists, row_idx, col_idx, max_width, max_height, crosses) do
+    if row_idx >= max_height or col_idx >= max_width do
+      crosses
+    else
+      connector = Enum.at(map, row_idx) |> String.at(col_idx)
+      coord = %Coord{col: col_idx, row: row_idx}
+      val = Map.get(dists, coord)
+
+      if val != nil and connector != "7" and connector != "L" do
+        fill_insides(map, dists, row_idx + 1, col_idx + 1, max_width, max_height, crosses + 1)
+      else
+        fill_insides(map, dists, row_idx + 1, col_idx + 1, max_width, max_height, crosses)
+      end
+    end
+  end
+
   def run do
     case File.read("input/day10.in") do
       {:ok, input} ->
-        String.split(input, "\n")
-        |> Enum.map(&String.graphemes/1)
-        |> process()
+        lines = String.split(input)
+
+        dists =
+          lines
+          |> Enum.map(&String.graphemes/1)
+          |> process()
+
+        # part one
+        dists
         |> Map.values()
         |> Enum.max()
+        |> IO.inspect()
+
+        width = List.first(lines) |> String.length()
+        height = length(lines)
+
+        lines
+        |> Enum.with_index()
+        |> Enum.map(fn {line, row_idx} ->
+          String.graphemes(line)
+          |> Enum.with_index()
+          |> Enum.reduce(0, fn {_, col_idx}, acc ->
+            coord = %Coord{col: col_idx, row: row_idx}
+            val = Map.get(dists, coord)
+
+            if val == nil do
+              crosses = fill_insides(lines, dists, row_idx, col_idx, width, height, 0)
+
+              if rem(crosses, 2) == 1 do
+                acc = acc + 1
+              else
+                acc
+              end
+            else
+              acc
+            end
+          end)
+        end)
+        |> Enum.sum()
         |> IO.inspect()
 
       {:error, reason} ->
