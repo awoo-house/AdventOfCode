41325181877ddc85cfd36a225cea0ea93842b772
Fox Huston <448633+foxhuston@users.noreply.github.com>
2023-12-18 09:20:46 -0500

    Day 15: Part II

--- fox/lib/Advent15/Advent15.ex
+++ fox/lib/Advent15/Advent15.ex
@@ -7,19 +7,75 @@ defmodule Advent15 do
   def run do
     case File.read("./lib/Puzz15.input.txt") do
       {:error, e} -> raise "Could not open file because: #{e}."
-      {:ok, inp} -> IO.puts("Hash Sum = #{hash_all(inp)}")
+      {:ok, inp} ->
+        # Part I
+        # IO.puts("Hash Sum = #{hash_all(inp)}")
+
+        # Part II
+        instrs = parse_instrs(inp)
+        map = run_hm_steps(instrs)
+        IO.puts("Lens Scores = #{get_lens_scores(map)}")
+    end
+  end
+
+  #### TYPES ###################################################################
+
+  @type holiday_hashmap() :: %{ integer() => Lens.t() }
+  @type hm_step() :: { :add, Lens.t() } | { :remove, charlist() }
+
+  #### INPUT ###################################################################
+
+
+  @spec parse_instrs(String.t()) :: list(hm_step())
+  def parse_instrs(inp) do
+    inp
+    |> String.trim
+    |> String.split(",")
+    |> Enum.map(&parse_instr/1)
+  end
+
+  @spec parse_instr(String.t()) :: hm_step()
+  def parse_instr(inp) do
+    strs = inp
+      |> String.split("=", parts: 2)
+      |> Enum.map(&to_charlist/1)
+
+    case strs do
+      [c]    -> {:remove, Enum.drop(c, -1)}
+      [k, [v]] -> {:add, Lens.new(k, v - ?0)}
+      o      -> raise "Parse instruction error on #{inp}"
     end
   end
 
   #### API #####################################################################
 
-  @type holiday_hashmap() :: %{ integer() => Lens.t() }
-  @type hm_step() :: { :add, Lens.t() } | { :remove, charlist() }
+  @spec get_lens_scores(holiday_hashmap()) :: integer()
+  def get_lens_scores(map) do
+    map
+    |> Enum.map(fn
+      {box_num, lenses} ->
+        lenses
+        |> Enum.with_index
+        |> Enum.reduce(0, fn
+          ({%Lens{ focal_length: focal_length }, idx}, acc) ->
+            ((1 + box_num) * (1 + idx) * focal_length) + acc
+          end
+        )
+    end)
+    |> Enum.sum
+  end
+
+  @spec run_hm_steps(holiday_hashmap(), list(hm_step())) :: holiday_hashmap()
+  def run_hm_steps(map \\ %{}, steps)
+  def run_hm_steps(map, []), do: map
+  def run_hm_steps(map, [s | steps]) do
+    run_hm_steps(do_hm_step(map, s), steps)
+  end
 
   @spec do_hm_step(holiday_hashmap(), hm_step()) :: holiday_hashmap()
   def do_hm_step(map, {:add, lens}) do
     Map.update(map, hash(lens.label), [lens],
-      fn vals -> vals ++ [lens] end
+      fn vals -> upsert_lens(lens, vals) end
     )
   end
   def do_hm_step(map, {:remove, key}) do
@@ -27,6 +83,11 @@ defmodule Advent15 do
       fn vals -> Enum.filter(vals, fn %Lens{ label: k } -> k !== key end) end
     )
   end
+
+  @spec upsert_lens(Lens.t(), list(Lens.t())) :: list(Lens.t())
+  def upsert_lens(l, []), do: [l]
+  def upsert_lens(l, [h | t]) when l.label == h.label, do: [l | t]
+  def upsert_lens(l, [h | t]), do: [h | upsert_lens(l, t)]
 
   @spec hash_all(String.t()) :: integer()
   def hash_all(str) do
--- fox/test/Advent15_test.exs
+++ fox/test/Advent15_test.exs
@@ -34,4 +34,51 @@ defmodule Advent15Test do
     IO.inspect(m)
   end
 
+  test "parse instr" do
+    assert parse_instr("rn=1") == {:add, Lens.new(~c"rn", 1)}
+    assert parse_instr("cm-")  == {:remove, ~c"cm"}
+  end
+
+  test "upsert lens" do
+    a0 = Lens.new(~c"a", 0)
+    b1 = Lens.new(~c"b", 1)
+    c2 = Lens.new(~c"c", 2)
+
+    a3 = Lens.new(~c"a", 3)
+
+    lenses = []
+
+    lenses = upsert_lens(a0, lenses)
+    assert lenses == [a0]
+
+    lenses = upsert_lens(b1, lenses)
+    assert lenses == [a0, b1]
+
+    lenses = upsert_lens(c2, lenses)
+    assert lenses == [a0, b1, c2]
+
+    lenses = upsert_lens(a3, lenses)
+    assert lenses == [a3, b1, c2]
+  end
+
+  test "run map" do
+    inp = parse_instrs("rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7")
+    map = run_hm_steps(inp)
+
+    expected = %{
+      0 => [Lens.new(~c"rn", 1), Lens.new(~c"cm", 2)],
+      1 => [],
+      3 => [Lens.new(~c"ot", 7), Lens.new(~c"ab", 5), Lens.new(~c"pc", 6)]
+    }
+
+    assert map == expected
+  end
+
+  test "get lens score" do
+    inp = parse_instrs("rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7")
+    map = run_hm_steps(inp)
+
+    assert get_lens_scores(map) == 145
+  end
+
 end
