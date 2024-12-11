7e7dc8d04009706c2f1f9484acc2ca0aa8474115
lexi Tramel <hi@lexi.sh>
2024-12-11 10:58:58 -0500

    lexi d7p2

--- 2024/lexi/p7.jl
+++ 2024/lexi/p7.jl
@@ -1,55 +1,58 @@
-
-
 struct Q 
   answer::Int64
   nums::Vector{Int64}
 end
 
-import Pkg
-Pkg.add("Combinatorics")
-using Combinatorics
 using Base.Iterators
 
-function get_all_possible_operators_in_spaces(num_spaces)::Matrix{Function}
-  e = [(*),(+)]
-  r = fill(e, num_spaces)
-  p = collect(Base.Iterators.product(r...))
-  display(p)
-  v = permutedims(vcat(p...))
-  
-  m::Vector{Vector{Function}} = []
-  for i in v
-    push!(m, collect(i))
-  end
-  hcat(m...)
-  # reshape(v, num_spaces, num_spaces)
+all_ops::Dict{Int64, Matrix{String}} = Dict()
+
+function (|)(i1::Int64, i2::Int64)::Int64 
+  parse(Int, string(i1, i2))
 end
 
-(function get_potential_solutions(q::Q)
+
+function get_all_possible_operators_in_spaces(num_spaces)::Matrix{String}
+  if !haskey(all_ops, num_spaces)
+    println("Need to calculate ops possibilities for $num_spaces")
+    e = ["*","+","|"]
+    r = fill(e, num_spaces)
+    p = Base.Iterators.product(r...)
+    v = permutedims(vcat(p...))
+    
+    m::Vector{Vector{String}} = []
+    for i in v
+      push!(m, collect(i))
+    end
+    ans = hcat(m...)
+    all_ops[num_spaces] = ans
+    println("Done calculating ops possibilities for $num_spaces")
+  end
+  all_ops[num_spaces]
+end
+
+function get_potential_solutions(q::Q)
   num_spaces = length(q.nums) - 1
   ops = get_all_possible_operators_in_spaces(num_spaces)
-
-  expressions = collect(zip(
-    q.nums[1:num_spaces],
-    q.nums[2:(num_spaces + 1)]
-  ))
-  display(expressions)
-  display(ops)
-
-  mapreduce(poss -> begin
-    display(poss)
-    display(expressions...)
-    broadcast(poss, expressions...)
-  end, ops)
-  
-  # Now we have expressions, a [num_spaces]x2 matrix, and
-  # ops, a [num_spaces]x[num_possibilites] matrix
-  # And for each j in num_possibilites, we want to apply
-  # (expressions[i][1],expressions[i][2]) to the function at ops[i][j]
-
-  # [f.(a) for (f, a) in ]
-
-end)(Q(190, [10;19]))
+  answers = mapslices(f -> begin
+    pushfirst!(f, "+")
+    zipped = zip(f,q.nums)
+    foldl((acc, (op, next_num)) -> begin 
+      if op === "*"
+        acc * next_num
+      elseif op === "|"
+        acc | next_num
+      else 
+        acc + next_num
+      end
+    end, zipped; init=0)
+  end, ops, dims=1)
+  if any(map(f -> f === q.answer, answers))
+    q.answer
+  else
+    0
+  end
+end
 
 function  get_data()
   input = open(io->read(io, String), "inputs/7.txt")
@@ -59,12 +62,12 @@ function  get_data()
     nums = map(c -> parse(Int, c), split(parts[2], " "))
     Q(parse(Int, parts[1]), nums)
   end, lines)
-  qs
-  
-  
+  d = 0
+  mapreduce(f -> begin 
+    d += 1
+    println("...$d...")
+    get_potential_solutions(f)
+  end , +, qs)
 end
-get_data()
 
-
-n::Tuple{Int64, Int64} = (75, 13)
-*(n...)
+display(get_data())
